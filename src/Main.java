1f04debeebf8cecb7c29fbf54393581ecf279c0a
Mr. Hernandez <mr.hernandez@SLA-vh.local>
2022-10-26 13:56:20 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi class!");
+        System.out.println("It's Mr. Hernandez.");
+        System.out.println("Who else is here?");
+    }
+}
